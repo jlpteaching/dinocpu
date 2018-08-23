a2dc785b3305caa0d899448097051f9d0a28523e
Jason Lowe-Power <jason@lowepower.com>
2018-08-23 11:19:11 -0700

    Add main control logic and tester
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- src/main/scala/single-cycle/control.scala
+++ src/main/scala/single-cycle/control.scala
@@ -3,6 +3,7 @@
 package edu.darchr.codcpu
 
 import chisel3._
+import chisel3.util.{BitPat, ListLookup}
 
 /**
  * Main control logic for our simple processor
@@ -10,6 +11,7 @@ import chisel3._
  * Describe the I/O here
  *
  * For more information, see section 4.4 of Patterson and Hennessy
+ * This follows figure 4.22
  */
 class Control extends Module {
   val io = IO(new Bundle {
@@ -20,15 +22,31 @@ class Control extends Module {
     val memtoreg = Output(Bool())
     val aluop = Output(UInt(2.W))
     val memwrite = Output(Bool())
-    val alusrc = Output(Bool())
     val regwrite = Output(Bool())
+    val alusrc = Output(Bool())
   })
 
-  io.branch := Bool(false)
-  io.memread := Bool(false)
-  io.memtoreg := Bool(false)
-  io.aluop := 0.U
-  io.memwrite := Bool(false)
-  io.alusrc := Bool(false)
-  io.regwrite := Bool(false)
+  val signals =
+    ListLookup(io.opcode,
+      /*default*/           List(0.U,    0.U,     0.U,       0.U,   0.U,     0.U,     0.U),
+      Array(                 /* branch, memread, memtoreg, aluop, memwrite, alusrc, regwrite */
+      // R-format
+      BitPat("b0110011") -> List(0.U,    0.U,     0.U,       2.U,   0.U,     0.U,     1.U),
+      // load
+      BitPat("b0000011") -> List(0.U,    1.U,     1.U,       0.U,   0.U,     1.U,     1.U),
+      // store
+      BitPat("b0100011") -> List(0.U,    0.U,     0.U,       0.U,   1.U,     1.U,     0.U),
+      // beq
+      BitPat("b1100011") -> List(1.U,    0.U,     0.U,       1.U,   0.U,     0.U,     0.U)  
+      ) // Array
+    ) // ListLookup
+
+  io.branch := signals(0)
+  io.memread := signals(1)
+  io.memtoreg := signals(2)
+  io.aluop := signals(3)
+  io.memwrite := signals(4)
+  io.alusrc := signals(5)
+  io.regwrite := signals(6)
+
 }
---
+++ src/test/scala/single-cycle/ControlUnitTest.scala
@@ -0,0 +1,88 @@
+// Unit tests for the ALU control logic
+
+package edu.darchr.codcpu
+
+import chisel3._
+
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+import Constants._
+
+class ControlUnitTester(c: Control) extends PeekPokeTester(c) {
+  private val ctl = c
+
+  // Copied from Patterson and Hennessy table 4.22
+  val tests = List( 
+    // Inputs,       alusrc, memtoreg, regwrite, memread, memwrite, branch, aluop 
+    ( "b0110011".U,    0,        0,       1,        0,       0,        0,     2),
+    ( "b0000011".U,    1,        1,       1,        1,       0,        0,     0),
+    ( "b0100011".U,    1,        0,       0,        0,       1,        0,     0),
+    ( "b1100011".U,    0,        0,       0,        0,       0,        1,     1)
+  )
+
+  for (t <- tests) {
+    poke(ctl.io.opcode, t._1)
+    step(1)
+    expect(ctl.io.branch, t._7)
+    expect(ctl.io.memread, t._5)
+    expect(ctl.io.memtoreg, t._3)
+    expect(ctl.io.aluop, t._8)
+    expect(ctl.io.memwrite, t._6)
+    expect(ctl.io.alusrc, t._2)
+    expect(ctl.io.regwrite, t._4)
+  }
+}
+
+/**
+  * This is a trivial example of how to run this Specification
+  * From within sbt use:
+  * {{{
+  * testOnly edu.darchr.codcpu.ControlTester
+  * }}}
+  * From a terminal shell use:
+  * {{{
+  * sbt 'testOnly edu.darchr.codcpu.ControlTester'
+  * }}}
+  */
+class ControlTester extends ChiselFlatSpec {
+  private val backendNames = if(firrtl.FileUtils.isCommandAvailable(Seq("verilator", "--version"))) {
+    Array("firrtl", "verilator")
+  }
+  else {
+    Array("firrtl")
+  }
+  for ( backendName <- backendNames ) {
+    "Control" should s"save written values (with $backendName)" in {
+      Driver(() => new Control, backendName) {
+        c => new ControlUnitTester(c)
+      } should be (true)
+    }
+  }
+
+  "Basic test using Driver.execute" should "be used as an alternative way to run specification" in {
+    iotesters.Driver.execute(Array(), () => new Control) {
+      c => new ControlUnitTester(c)
+    } should be (true)
+  }
+
+  "using --backend-name verilator" should "be an alternative way to run using verilator" in {
+    if(backendNames.contains("verilator")) {
+      iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new Control) {
+        c => new ControlUnitTester(c)
+      } should be(true)
+    }
+  }
+
+  "running with --is-verbose" should "show more about what's going on in your tester" in {
+    iotesters.Driver.execute(Array("--is-verbose"), () => new Control) {
+      c => new ControlUnitTester(c)
+    } should be(true)
+  }
+
+  "running with --fint-write-vcd" should "create a vcd file from your test" in {
+    iotesters.Driver.execute(Array("--fint-write-vcd"), () => new Control) {
+      c => new ControlUnitTester(c)
+    } should be(true)
+  }
+}
