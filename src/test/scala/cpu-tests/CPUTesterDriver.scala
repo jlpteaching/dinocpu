6fea57fc40581df797208f01ccdb8317e0135510
Jason Lowe-Power <jason@lowepower.com>
2018-12-17 18:51:45 -0800

    Add tests for five cycle and pipelined CPU
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- src/test/scala/cpu-tests/CPUTesterDriver.scala
+++ src/test/scala/cpu-tests/CPUTesterDriver.scala
@@ -106,44 +106,6 @@ case class CPUTestCase(
 }
 
 object CPUTesterDriver {
-  val testCases = List[CPUTestCase](
-    CPUTestCase("add1",   1,  Map(5 -> 1234),
-                              Map(0 -> 0, 5 -> 1234, 6 -> 1234),
-                              Map(), Map()),
-    CPUTestCase("add2",   1,  Map(5 -> 1234, 20 -> 5678),
-                              Map(0 -> 0, 10 -> 6912),
-                              Map(), Map()),
-    CPUTestCase("add0",   1,  Map(5 -> 1234, 6 -> 3456),
-                              Map(0 -> 0, 5 -> 1234, 6 -> 3456),
-                              Map(), Map()),
-    CPUTestCase("addfwd", 10, Map(5 -> 1, 10 -> 0),
-                              Map(5 -> 1, 10 -> 10),
-                              Map(), Map()),
-    CPUTestCase("and",    1,  Map(5 -> 1234, 6 -> 5678),
-                              Map(7 -> 1026),
-                              Map(), Map()),
-    CPUTestCase("beq",    3,  Map(5 -> 1234, 6 -> 1, 7 -> 5678, 8 -> 9012),
-                              Map(5 -> 0, 6 -> 1, 7 -> 5678, 8 -> 9012),
-                              Map(), Map(), "False"),
-    CPUTestCase("beq",    3,  Map(5 -> 1234, 6 -> 1, 7 -> 5678, 28 -> 5678),
-                              Map(5 -> 1235, 6 -> 1, 7 -> 5678, 28 -> 5678),
-                              Map(), Map(), "True"),
-    CPUTestCase("lw1",    1,  Map(),
-                              Map(5 -> BigInt("ffffffff", 16)),
-                              Map(), Map()),
-    CPUTestCase("lwfwd",  2,  Map(5 -> BigInt("ffffffff", 16), 10 -> 5),
-                              Map(5 -> 1, 10 -> 6),
-                              Map(), Map()),
-    CPUTestCase("or",     1,  Map(5 -> 1234, 6 -> 5678),
-                              Map(7 -> 5886),
-                              Map(), Map()),
-    CPUTestCase("sub",    1,  Map(5 -> 1234, 6 -> 5678),
-                              Map(7 -> BigInt("FFFFEEA4", 16)),
-                              Map(), Map()),
-    CPUTestCase("sw",     6,  Map(5 -> 1234),
-                              Map(6 -> 1234),
-                              Map(), Map(0x100 -> 1234))
-  )
   def apply(testCase: CPUTestCase, cpuType: String): Boolean = {
     val driver = new CPUTesterDriver(cpuType, testCase.binary)
     driver.initRegs(testCase.initRegs)
