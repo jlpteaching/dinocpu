a8c0f4fd16f3c6747dcc4f76d0625af0d1cc6675
Jason Lowe-Power <jason@lowepower.com>
2019-01-17 08:46:31 -0800

    Update build deps to checkout at the right time
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- get_and_build_deps.sh
+++ get_and_build_deps.sh
@@ -7,27 +7,32 @@
 pushd /opt
 
 git clone https://github.com/freechipsproject/firrtl.git
-git checkout df3a34f01d227ff9ad0e63a41ff10001ac01c01d
 git clone https://github.com/freechipsproject/firrtl-interpreter.git
-git checkout df3a34f01d227ff9ad0e63a41ff10001ac01c01d
 git clone https://github.com/freechipsproject/chisel3.git
-git checkout 685790b2c6c7ff8ddfd34f2f84572a985d3416cc
 git clone https://github.com/freechipsproject/chisel-testers.git
-git checkout 8a737012f3c93dd41a3a9e120eebfc07ba541ebf
 git clone https://github.com/freechipsproject/treadle.git
-git checkout a10de08ff4dca3f5db44a665ca807d4457f0f10e
 
 # Now, compile each and install to /opt/ivy2/local
 cd firrtl && \
-sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal && \
+git checkout df3a34f01d227ff9ad0e63a41ff10001ac01c01d && \
+sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal
+
 cd ../firrtl-interpreter && \
-sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal && \
+git checkout df3a34f01d227ff9ad0e63a41ff10001ac01c01d && \
+sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal
+
 cd ../chisel3 && \
-sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal && \
+git checkout 685790b2c6c7ff8ddfd34f2f84572a985d3416cc && \
+sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal
+
 cd ../chisel-testers && \
-sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal && \
+git checkout 8a737012f3c93dd41a3a9e120eebfc07ba541ebf && \
+sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal
+
 cd ../treadle && \
-sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal && \
+git checkout a10de08ff4dca3f5db44a665ca807d4457f0f10e && \
+sbt -ivy /opt/ivy2 compile && sbt -ivy /opt/ivy2 publishLocal
+
 cd ..
 
 rm -rf firrtl firrtl-interpreter chisel3 chisel-testers treadle
