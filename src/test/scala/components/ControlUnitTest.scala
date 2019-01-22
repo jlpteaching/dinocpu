bef6a381f16cd315e5ec23eefadb17c340998149
filipeeduardo <fborges@ucdavis.edu>
2019-01-22 00:52:24 -0800

    added additional tests for control unit

--- src/test/scala/components/ControlUnitTest.scala
+++ src/test/scala/components/ControlUnitTest.scala
@@ -1,4 +1,4 @@
-// Unit tests for the ALU control logic
+// Unit tests for the main control logic
 
 package dinocpu
 
@@ -11,11 +11,18 @@ class ControlUnitTester(c: Control) extends PeekPokeTester(c) {
   private val ctl = c
 
   val tests = List(
-    // Inputs,       imm,      toreg, regwrite, memread, memwrite, branch, add
-    ( "b0110011".U,    0,        0,       1,        0,       0,        0,     0), // R-type
-    ( "b0000011".U,    1,        1,       1,        1,       0,        0,     1), // Load
-    ( "b0100011".U,    1,        0,       0,        0,       1,        0,     1), // Store
-    ( "b1100011".U,    0,        0,       0,        0,       0,        1,     0)  // beq
+    // Inputs,       imm,      toreg, regwrite, memread, memwrite, branch, add,      alusrc1, jump 
+    ( "b0110011".U,    0,        0,       1,        0,       0,        0,     0,	0,	0), // R-type
+    ( "b0010011".U,    1,        0,       1,        0,       0,        0,     0,	0,	0), // I-type
+    ( "b0000011".U,    1,        1,       1,        1,       0,        0,     1,	0,	0), // Load
+    ( "b0100011".U,    1,        0,       0,        0,       1,        0,     1,	0,	0), // Store
+    ( "b1100011".U,    0,        0,       0,        0,       0,        1,     0,	0,	0),  // beq
+    ( "b0110111".U,    1,        0,       1,        0,       0,        0,     1,	1,	0), // lui
+    ( "b0010111".U,    1,        0,       1,        0,       0,        0,     1,	2,	0), // auipc
+    ( "b1101111".U,    0,        2,       1,        0,       0,        1,     0,	1,	2), // jal
+    ( "b1100111".U,    1,        2,       1,        0,       0,        0,     0,	0,	3) // jalr
+
+
   )
 
   for (t <- tests) {
@@ -28,6 +35,8 @@ class ControlUnitTester(c: Control) extends PeekPokeTester(c) {
     expect(ctl.io.memwrite, t._6)
     expect(ctl.io.immediate, t._2)
     expect(ctl.io.regwrite, t._4)
+    expect(ctl.io.alusrc1,t._9)
+    expect(ctl.io.jump,t._10)
   }
 }
 
