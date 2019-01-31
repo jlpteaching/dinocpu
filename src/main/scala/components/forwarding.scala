79ae54c2593138ae6ab05f21649b810127041265
Jason Lowe-Power <jason@lowepower.com>
2019-01-31 14:48:09 -0800

    Add documentation to forwarding
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- src/main/scala/components/forwarding.scala
+++ src/main/scala/components/forwarding.scala
@@ -7,7 +7,17 @@ import chisel3._
 /**
  * The Forwarding unit
  *
- * Here we should describe the I/O
+ * Input:  rs1, the register number for which the data is used in the execute stage
+ * Input:  rs2, the register number for which the data is used in the execute stage
+ * Input:  exmemrd, the destination register for the instruction in the mem stage
+ * Input:  exmemrw, true if the bit in the memory stage needs to write the register file
+ * Input:  memwbrd, the destination register for the instruction in the writeback stage
+ * Input:  memwbrw, true if the bit in the writeback stage needs to write the register file
+ *
+ * Output: forwardA, 0, don't forward. 1, forward from mem stage. 2, forward from wb stage.
+ *                   This is used for the "readdata1" forwarding
+ * Output: forwardB, 0, don't forward. 1, forward from mem stage. 2, forward from wb stage.
+ *                   This is used for the "readdata2" forwarding
  *
  * For more information, see Section 4.7 of Patterson and Hennessy
  * This follows figure 4.53
@@ -22,7 +32,6 @@ class ForwardingUnit extends Module {
     val memwbrd = Input(UInt(5.W))
     val memwbrw = Input(Bool())
 
-
     val forwardA = Output(UInt(2.W))
     val forwardB = Output(UInt(2.W))
   })
@@ -32,7 +41,7 @@ class ForwardingUnit extends Module {
   } .elsewhen (io.rs1 === io.memwbrd && io.memwbrd =/= 0.U && io.memwbrw && ~(io.exmemrw && io.exmemrd =/=0.U && io.exmemrd ===io.rs1)) {
     io.forwardA := 2.U
   }
-  } 
+  }
 .otherwise {
     io.forwardA := 0.U
   }
