5a3aed5f7292d8fd5e45489158f62b63111aeef1
Jason Lowe-Power <jason@lowepower.com>
2019-03-05 09:41:07 -0800

    Add other dockerfiles
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ install-intellij.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# This script is from https://stackoverflow.com/questions/30130934/how-to-install-intellij-idea-on-ubuntu
+
+echo "Installing IntelliJ IDEA..."
+
+# Or U for ultimate
+ed=C
+
+# Fetch the most recent version
+VERSION=$(wget "https://www.jetbrains.com/intellij-repository/releases" -qO- | grep -P -o -m 1 "(?<=https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/BUILD/)[^/]+(?=/)")
+
+# Prepend base URL for download
+URL="https://download.jetbrains.com/idea/ideaI$ed-$VERSION.tar.gz"
+
+echo $URL
+
+# Truncate filename
+FILE=$(basename ${URL})
+
+# Set download directory
+DEST=/$FILE
+
+echo "Downloading idea-I$ed-$VERSION to $DEST..."
+
+# Download binary
+wget -cO ${DEST} ${URL} --read-timeout=5 --tries=0
+
+echo "Download complete!"
+
+# Set directory name
+DIR="/opt/idea-I$ed-$VERSION"
+
+echo "Installing to $DIR"
+
+# Untar file
+if mkdir ${DIR}; then
+    tar -xzf ${DEST} -C ${DIR} --strip-components=1
+fi
+
+# Grab executable folder
+BIN="$DIR/bin"
+
+# Add permissions to install directory
+chmod -R +rwx ${DIR}
+
+# Create symlink entry
+ln -s ${BIN}/idea.sh /usr/local/bin/idea
+
+echo "Done."
