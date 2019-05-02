491c5630acbb893ba932f8ddae0f35160e5131e9
Nima Ganjehloo <nganjehl@amarillo.cs.ucdavis.edu>
2019-05-01 20:30:40 -0700

    cleaned generated verilog and verilator

--- src/verilog/obj_dir/VTop__ALLsup.d
+++
@@ -1,5 +0,0 @@
-VTop__ALLsup.o: VTop__ALLsup.cpp VTop__Syms.cpp VTop__Syms.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h VTop.h
