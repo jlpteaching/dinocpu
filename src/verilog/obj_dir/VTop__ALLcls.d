55fba924bf58dff35cd607c82d0f7c055e13cb37
Nima Ganjehloo <nganjehl@amarillo.cs.ucdavis.edu>
2019-05-01 20:30:40 -0700

    csr implemented, needs testing, cleaned verilog stuff

--- src/verilog/obj_dir/VTop__ALLcls.d
+++
@@ -1,5 +0,0 @@
-VTop__ALLcls.o: VTop__ALLcls.cpp VTop.cpp VTop.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h VTop__Syms.h
--- src/verilog/obj_dir/VTop__ver.d
+++
@@ -1,1 +0,0 @@
-obj_dir/VTop.cpp obj_dir/VTop.h obj_dir/VTop.mk obj_dir/VTop__Syms.cpp obj_dir/VTop__Syms.h obj_dir/VTop__ver.d obj_dir/VTop_classes.mk  : /usr/bin/verilator_bin   /usr/bin/verilator_bin Top.v 
