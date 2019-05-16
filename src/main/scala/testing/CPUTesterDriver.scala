336d7f3ad8bd4268fe3337e9bcaa121e1e9accf6
Nima Ganjehloo <nganjehl@amarillo.cs.ucdavis.edu>
2019-05-15 20:10:33 -0700

    Removed CSR stuff for time being until we find a good way of testing it

--- src/main/scala/testing/CPUTesterDriver.scala
+++ src/main/scala/testing/CPUTesterDriver.scala
@@ -59,12 +59,6 @@ class CPUTesterDriver(cpuType: String,
     simulator.reset(5)
   }
 
-  def initCSR(vals: Map[Int, BigInt]) {
-    for ((num, value) <- vals) {
-      simulator.poke(s"cpu.csr.read_mapping_$num", value)
-    }
-  }
-
   def initRegs(vals: Map[Int, BigInt]) {
     for ((num, value) <- vals) {
       simulator.poke(s"cpu.registers.regs_$num", value)
@@ -79,22 +73,6 @@ class CPUTesterDriver(cpuType: String,
     for ((addr, value) <- vals) {
       simulator.pokeMemory(s"cpu.mem.memory", addr, value)
     }
-  }
-
-  def checkCSR(vals: Map[Int, BigInt]): Boolean = {
-    var success = true
-    for ((num, value) <- vals) {
-      try {
-        simulator.expect(s"cpu.csr.read_mapping_$num", value)
-      } catch {
-        case _: TreadleException => {
-          success = false
-          val real = simulator.peek(s"cpu.csr.read_mapping_$num")
-          println(s"CSR $num failed to match. Was $real. Should be $value")
-        }
-      }
-    }
-    success
   }
 
   def checkRegs(vals: Map[Int, BigInt]): Boolean = {
@@ -150,8 +128,6 @@ class CPUTesterDriver(cpuType: String,
 case class CPUTestCase(
   binary:  String,
   cycles: Map[String, Int],
-  initCSR: Map[Int, BigInt],
-  checkCSR: Map[Int, BigInt],
   initRegs: Map[Int, BigInt],
   checkRegs: Map[Int, BigInt],
   initMem: Map[Int, BigInt],
@@ -167,11 +143,9 @@ case class CPUTestCase(
 object CPUTesterDriver {
   def apply(testCase: CPUTestCase, cpuType: String, branchPredictor: String = ""): Boolean = {
     val driver = new CPUTesterDriver(cpuType, branchPredictor, testCase.binary, testCase.extraName)
-    driver.initCSR(testCase.initCSR)
-    driver.initRegs(testCase.initRegs)
     driver.initMemory(testCase.initMem)
     driver.run(testCase.cycles(cpuType))
-    val success = driver.checkCSR(testCase.checkCSR)
-    success && driver.checkRegs(testCase.checkRegs) && driver.checkMemory(testCase.checkMem)
+    val success = driver.checkRegs(testCase.checkRegs)
+    success && driver.checkMemory(testCase.checkMem)
   }
 }
