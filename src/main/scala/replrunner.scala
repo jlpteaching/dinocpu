e1b39a2f9e8507ede9c7962960baf3ea15cf2a30
Daniel <daniel.d.j.grau@gmail.com>
2019-05-28 19:32:49 -0700

    cleaned code, added some comments

--- src/main/scala/replrunner.scala
+++ src/main/scala/replrunner.scala
@@ -3,25 +3,7 @@ import treadle.TreadleOptionsManager
 import treadle.repl.HasReplConfig
 
 object replrunner {
-  val helptext = "usage: singlestep <test name> <CPU type>"
-
-  def initRegs(vals: Map[Int, BigInt],repl: treadle.TreadleRepl ) {
-    for ((num, value) <- vals) {
-      repl.currentTreadleTester.poke(s"cpu.registers.regs_$num", value)
-    }
-  }
-
-  /**
-    *
-    * @param vals holds "addresses" to values. Where address is the nth *word*
-    */
-  def initMemory(vals: Map[Int, BigInt],repl: treadle.TreadleRepl): Unit = {
-    for ((addr, value) <- vals) {
-        repl.currentTreadleTester.pokeMemory(s"cpu.mem.memory", addr, value)
-    }
-  }
-
-
+  val helptext = "usage: replrunner <test name> <CPU type>"
 
   def main(args: Array[String]): Unit = {
     require(args.length >= 2, "Error: Expected at least two argument\n" + helptext)
@@ -39,17 +21,22 @@ object replrunner {
         ""
       }
 
+    //setup test
     val driver = new CPUTesterDriver(cpuType, predictor, test.binary, test.extraName, true)
     driver.initRegs(test.initRegs)
     driver.initMemory(test.initMem)
 
+    //create a copy of the options from the testerdriver, just as slightly different type
     val options = new TreadleOptionsManager with HasReplConfig
     options.treadleOptions = driver.optionsManager.treadleOptions.copy()
     options.firrtlOptions = driver.optionsManager.firrtlOptions.copy()
 
+    //copy the configured simulator in the REPL
     val repl = new treadle.TreadleRepl(options)
     repl.currentTreadleTesterOpt = Some(driver.simulator)
     repl.run()
+
+    //check test result
     if (driver.checkRegs(test.checkRegs) && driver.checkMemory(test.checkMem)) {
       println("Test passed!")
     } else {
