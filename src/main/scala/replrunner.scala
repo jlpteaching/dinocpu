d314f9e7ffdb713a7ed496b5b72efee8de3995fe
Daniel <daniel.d.j.grau@gmail.com>
2019-06-03 10:57:30 -0700

    documented function and example usage

--- src/main/scala/replrunner.scala
+++ src/main/scala/replrunner.scala
@@ -2,14 +2,25 @@ package dinocpu
 import treadle.TreadleOptionsManager
 import treadle.repl.HasReplConfig
 
+/** Wrapper object to run the treadle REPL for debugging CPU designs
+  * Use main function to call
+  */
 object replrunner {
   val helptext = "usage: replrunner <test name> <CPU type>"
 
+  /** Runs a test in an interactive environment allows single steping and reading register values
+    * params test_name CPU_type
+    * example usage
+    * {{{
+    * runMain replrunner add1 pipelined
+    * }}}
+    */
   def main(args: Array[String]): Unit = {
     require(args.length >= 2, "Error: Expected at least two argument\n" + helptext)
 
     println(s"Running test ${args(0)} on CPU design ${args(1)}")
 
+    //get the params, test name and cpu type
     val test = InstTests.nameMap(args(0))
     val params = args(1).split(":")
     val cpuType = params(0)
@@ -26,7 +37,7 @@ object replrunner {
     driver.initRegs(test.initRegs)
     driver.initMemory(test.initMem)
 
-    //create a copy of the options from the testerdriver, just as slightly different type
+    //create a copy of the options from the testerdriver, with the replconfig option
     val options = new TreadleOptionsManager with HasReplConfig
     options.treadleOptions = driver.optionsManager.treadleOptions.copy()
     options.firrtlOptions = driver.optionsManager.firrtlOptions.copy()
@@ -34,6 +45,7 @@ object replrunner {
     //copy the configured simulator in the REPL
     val repl = new treadle.TreadleRepl(options)
     repl.currentTreadleTesterOpt = Some(driver.simulator)
+
     repl.run()
 
     //check test result
