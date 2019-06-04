144396f728c53e22e1ab5133806debd840299271
Jason Lowe-Power <jason@lowepower.com>
2019-06-03 18:37:58 -0700

    Convert PartialWrite to OutstandingReq
    This treats reads and write the same at the port interface. Now, you
    also store the address, etc. when there is an outstanding read and use
    this data when the response returns.
    
    Note: There's a nasty edge case if you have write followed by something
    else. You cannot send the next request (read) until after one cycle so
    the write part of the write request can be sent. I don't think the tests
    test for this.
    
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- src/main/scala/components/memory-async.scala
+++ src/main/scala/components/memory-async.scala
@@ -20,7 +20,7 @@ object MemoryOperation {
 import MemoryOperation._
 
 // A Bundle used for representing a memory access by instruction memory or data memory.
-class Request extends Bundle {  
+class Request extends Bundle {
   val address      = UInt(32.W)
   val writedata    = UInt(32.W)
   val operation    = UInt(1.W)
@@ -28,21 +28,18 @@ class Request extends Bundle {
 
 // A bundle used for representing the memory's response to a memory port.
 class Response extends Bundle {
-  // Offset is set to the least 2 significant bits of the address, and
-  // is mainly used to return the specific byte/halfword being addressed.
-  val offset       = UInt(2.W)
   // The 4-byte-wide block of data being returned by memory
   val data         = UInt(32.W)
 }
 
-/** 
+/**
  * The generic interface for communication between the IMem/DMemPort modules and the backing memory.
  *
  * Input:  request, the ready/valid interface for a MemPort module to issue Requests to. Memory
  *         will only accept a request when both request.valid (the MemPort is supplying valid data)
  *         and request.ready (the memory is idling for a request) are high.
  *
- * Output: response, the valid interface for the data outputted by memory if it was requested to read. 
+ * Output: response, the valid interface for the data outputted by memory if it was requested to read.
  *         the bits in response.bits should only be treated as valid data when response.valid is high.
  */
 class AsyncMemIO extends Bundle {
@@ -51,9 +48,9 @@ class AsyncMemIO extends Bundle {
 }
 
 /**
- * The modified asynchronous form of the dual ported memory module. 
+ * The modified asynchronous form of the dual ported memory module.
  * When io.imem.request.valid or io.imem.request.valid is true and the memory is ready for an operation,
- * this memory module simulates the latency of real DRAM by pushing memory accesses into pipes that delay 
+ * this memory module simulates the latency of real DRAM by pushing memory accesses into pipes that delay
  * the request for a configurable latency.
  *
  * As with the synced memory module, this memory should only be instantiated in the Top file,
@@ -62,16 +59,16 @@ class AsyncMemIO extends Bundle {
  * The I/O for this module is defined in [[AsyncMemIO]].
  */
 class DualPortedAsyncMemory(size: Int, memfile: String, latency: Int) extends Module {
-  def wireMemPipe(portio: AsyncMemIO, pipe: Pipe[Request]): Unit = {      
+  def wireMemPipe(portio: AsyncMemIO, pipe: Pipe[Request]): Unit = {
     pipe.io.enq.bits      <> DontCare
     pipe.io.enq.valid     := false.B
     portio.response.valid := false.B
-  
+
     // Memory is technically always ready, but we want to use the
     // ready/valid interface so that if needed we can restrict
     // executing memory operations
     portio.request.ready := true.B
-  } 
+  }
 
   val io = IO(new Bundle {
     val imem = new AsyncMemIO
@@ -100,11 +97,12 @@ class DualPortedAsyncMemory(size: Int, memfile: String, latency: Int) extends Mo
 
   when (imemPipe.io.deq.valid) {
     // We should only be expecting a read from instruction memory
-    assert(imemPipe.io.deq.bits.operation === Read) 
+    assert(imemPipe.io.deq.bits.operation === Read)
     val outRequest = imemPipe.io.deq.asTypeOf (new Request)
+    // Check that address is pointing to a valid location in memory
+    assert (outRequest.address < size.U)
     io.imem.response.valid        := true.B
     io.imem.response.bits.data    := memory(outRequest.address >> 2)
-    io.imem.response.bits.offset  := outRequest.address (1,0) 
   } .otherwise {
     // The memory's response can't possibly be valid if the imem pipe isn't outputting a valid request
     io.imem.response.valid := false.B
@@ -117,7 +115,7 @@ class DualPortedAsyncMemory(size: Int, memfile: String, latency: Int) extends Mo
   wireMemPipe(io.dmem, dmemPipe)
 
   when (io.dmem.request.valid) {
-    // Put the Request into the data pipe and signal that data memory is busy 
+    // Put the Request into the data pipe and signal that data memory is busy
     val inRequest = io.dmem.request.asTypeOf (new Request)
     dmemPipe.io.enq.bits  := inRequest
     dmemPipe.io.enq.valid := true.B
@@ -131,12 +129,11 @@ class DualPortedAsyncMemory(size: Int, memfile: String, latency: Int) extends Mo
     val address = outRequest.address >> 2
     // Check that address is pointing to a valid location in memory
     assert (outRequest.address < size.U)
-    
+
     when (outRequest.operation === Read) {
       io.dmem.response.valid        := true.B
       io.dmem.response.bits.data    := memory(address)
-      io.dmem.response.bits.offset  := outRequest.address (1,0)
-    } .elsewhen (outRequest.operation === Write) {  
+    } .elsewhen (outRequest.operation === Write) {
       io.dmem.response.valid        := false.B
       memory(address) := outRequest.writedata
     }
--- src/main/scala/components/memory-ports.scala
+++ src/main/scala/components/memory-ports.scala
@@ -11,11 +11,13 @@ import firrtl.annotations.MemoryLoadFileType
 
 import MemoryOperation._
 
-// A Bundle used for temporarily storing the necessary information for a write in the data memory accessor.
-class PartialWrite extends Bundle {
+// A Bundle used for temporarily storing the necessary information for a  read/write in the data memory accessor.
+class OutstandingReq extends Bundle {
   val address   = UInt(32.W)
   val writedata = UInt(32.W)
   val maskmode  = UInt(2.W)
+  val operation = UInt(1.W)
+  val sext      = Bool()
 }
 
 /**
@@ -124,97 +126,103 @@ class DMemPort extends Module {
   io      := DontCare
   io.good := io.response.valid
 
-  val storedWrite = RegInit(0.U.asTypeOf(Valid(new PartialWrite)))
+  // A register to hold intermediate data (e.g., write data, mask mode) while the request
+  // is outstanding to memory.
+  val outstandingReq = RegInit(0.U.asTypeOf(Valid(new OutstandingReq)))
+
+  // Used to set the valid bit of the outstanding request
+  val sending = Wire(Bool())
 
   // When the pipeline is supplying a valid read OR write request, send out the request
-  // ... on the condition that there isn't a stored write in the queue.
-  // We need to process stored writes first to guarantee atomicity of the memory write operation
-
-  when (io.valid && ! storedWrite.valid && (io.memread || io.memwrite)) {
+  // ... on the condition that there isn't an outstanding request in the queue.
+  // We need to process outstanding request first to guarantee atomicity of the memory write operation
+  // Ready if either we don't have an outstanding request or the outstanding request is a read and
+  // it has been satisfied this cycle. Note: we cannot send a read until one cycle after the write has
+  // been sent.
+  val ready = !outstandingReq.valid || (io.response.valid && (outstandingReq.valid && outstandingReq.bits.operation === Read))
+  when (io.valid && (io.memread || io.memwrite) && ready) {
     // Check if we aren't issuing both a read and write at the same time
     assert (! (io.memread && io.memwrite))
 
-    // On either a read or write we must read a whole block from memory.
-    //
-    // If the operation is a read, then we simply output the memory's
-    // response and all is good.
-    // If the operation is a write, then we have to read the existing
-    // memory at that position for any masking so that we support
-    // writing bytes or halfwords into memory.
+    // On either a read or write we must read a whole block from memory. Store the necessary
+    // information to redirect the memory's response back into itself through a write
+    // operation and get the right subset of the block on a read.
+    outstandingReq.bits.address   := io.address
+    outstandingReq.bits.writedata := io.writedata
+    outstandingReq.bits.maskmode  := io.maskmode
+    outstandingReq.bits.sext      := io.sext
     when (io.memwrite) {
-      // Store the necessary information to redirect the memory's response
-      // back into itself through a write operation
-      storedWrite.bits.address   := io.address
-      storedWrite.bits.writedata := io.writedata
-      storedWrite.bits.maskmode  := io.maskmode
-      storedWrite.valid := true.B
+      outstandingReq.bits.operation := Write
     } .otherwise {
-      storedWrite.valid := false.B
+      outstandingReq.bits.operation := Read
     }
-    // Program memory to perform a read
+    sending := true.B
+
+    // Program memory to perform a read. Always read since we must read before write.
     io.request.bits.address   := io.address
     io.request.bits.writedata := 0.U
     io.request.bits.operation := Read
     io.request.valid          := true.B
   } .otherwise {
+    // no request coming in so don't send a request out
     io.request.valid := false.B
+    sending := false.B
   }
 
+  // Response path
   when (io.response.valid) {
-    when (storedWrite.valid) {
+    assert(outstandingReq.valid)
+    when (outstandingReq.bits.operation === Write) {
       val writedata = Wire (UInt (32.W))
 
       // When not writing a whole word
-      when (storedWrite.bits.maskmode =/= 2.U) {
+      when (outstandingReq.bits.maskmode =/= 2.U) {
         // Read in the existing piece of data at the address, so we "overwrite" only part of it
-        val offset = storedWrite.bits.address (1, 0)
+        val offset = outstandingReq.bits.address (1, 0)
         val readdata = Wire (UInt (32.W))
         readdata := io.response.bits.data
         val data = Wire (UInt (32.W))
         // Mask the portion of the existing data so it can be or'd with the writedata
-        when (storedWrite.bits.maskmode === 0.U) {
+        when (outstandingReq.bits.maskmode === 0.U) {
           data := io.response.bits.data & ~(0xff.U << (offset * 8.U))
         } .otherwise {
           data := io.response.bits.data & ~(0xffff.U << (offset * 8.U))
         }
-        writedata := data | (storedWrite.bits.writedata << (offset * 8.U))
+        writedata := data | (outstandingReq.bits.writedata << (offset * 8.U))
       } .otherwise {
         // Write the entire word
-        writedata := storedWrite.bits.writedata
+        writedata := outstandingReq.bits.writedata
       }
 
-      // Program the memory to issue a write
+      // Program the memory to issue a write.
       val request = Wire (new Request)
-      request.address   := storedWrite.bits.address
+      request.address   := outstandingReq.bits.address
       request.writedata := writedata
       request.operation := Write
       io.request.bits  := request
       io.request.valid := true.B
-
-      // Mark the stored write register as being invalid.
-      storedWrite.valid := false.B
     } .otherwise {
       // Response is valid and we don't have a stored write.
       // Perform masking and sign extension on read data when memory is outputting it
       val readdata_mask      = Wire(UInt(32.W))
       val readdata_mask_sext = Wire(UInt(32.W))
 
-      val offset = io.response.bits.offset
-      when (io.maskmode === 0.U) {
+      val offset = outstandingReq.bits.address(1,0)
+      when (outstandingReq.bits.maskmode === 0.U) {
         // Byte
         readdata_mask := (io.response.bits.data >> (offset * 8.U)) & 0xff.U
-      } .elsewhen (io.maskmode === 1.U) {
+      } .elsewhen (outstandingReq.bits.maskmode === 1.U) {
         // Half-word
         readdata_mask := (io.response.bits.data >> (offset * 8.U)) & 0xffff.U
       } .otherwise {
         readdata_mask := io.response.bits.data
       }
 
-      when (io.sext) {
-        when (io.maskmode === 0.U) {
+      when (outstandingReq.bits.sext) {
+        when (outstandingReq.bits.maskmode === 0.U) {
           // Byte sign extension
           readdata_mask_sext := Cat(Fill(24, readdata_mask(7)),  readdata_mask(7, 0))
-        } .elsewhen (io.maskmode === 1.U) {
+        } .elsewhen (outstandingReq.bits.maskmode === 1.U) {
           // Half-word sign extension
           readdata_mask_sext := Cat(Fill(16, readdata_mask(15)), readdata_mask(15, 0))
         } .otherwise {
@@ -227,5 +235,10 @@ class DMemPort extends Module {
 
       io.readdata := readdata_mask_sext
     }
+    // Mark the outstanding request register as being invalid, unless sending
+    outstandingReq.valid := sending
+  } .otherwise {
+    // Keep the outstanding request valid or invalid unless sending
+    outstandingReq.valid := outstandingReq.valid | sending
   }
 }
