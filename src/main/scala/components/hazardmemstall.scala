f14b97378c56186b47f1bf17521a3bee40b39c41
Jared Barocsi <deyahruhd@gmail.com>
2019-12-26 20:36:22 -0800

    Update mem stall hazard detector with new stall logic

--- src/main/scala/components/hazardmemstall.scala
+++ src/main/scala/components/hazardmemstall.scala
@@ -13,51 +13,53 @@ import chisel3._
  * Input:  idex_rd, the register number of the destination register for the instruction in the ID/EX register
  * Input:  exmem_taken, if true, then we are using the nextpc in the EX/MEM register, *not* pc+4.
  * Input:  imem_good, high if instruction memory is idle and ready.
- *         If either imem_good or dmem_good are false then CPU will stall
  * Input:  dmem_good, high if data memory is idle and ready.
  *
  * Output: pcwrite, the value to write to the pc. If 0, pc+4, if 1 the next_pc from the memory stage,
  *         if 2, then the last pc value (2 stalls the pipeline)
- * Output: ifid_bubble, if true, we should instet a bubble in the IF/ID stage
- * Output: idex_bubble, if true, we should insert a bubble in the ID/EX stage
- * Output: exmem_bubble, if true, we should insert a bubble in the EX/MEM stage
+ * Output: imem_disable, if true, prevent instruction memory from receiving requests
+ * Output: ifid_bubble, if true, we should insert a bubble in the IF/ID stage
+ * Output: ifid_disable, if true, explicitly prevent the IF/ID register from being written to
  * Output: ifid_flush, if true, set the IF/ID register to 0
+ * Output: idex_bubble, if true, we should insert a bubble in the ID/EX stage
+ * Output: idex_disable, if true, explicitly prevent the ID/EX register from being written to
+ * Output: exmem_bubble, if true, we should insert a bubble in the EX/MEM stage
+ * Output: exmem_disable, if true, explicitly prevent the EX/MEM register from being written to
  *
  * For more information, see Section 4.7 and beginning of 4.8 of Patterson and Hennessy
  * This follows the "Data hazards and stalls" section and the "Assume branch not taken" section
  */
 class HazardUnitMemStall extends Module {
   val io = IO(new Bundle {
-    val rs1          = Input(UInt(5.W))
-    val rs2          = Input(UInt(5.W))
-    val idex_memread = Input(Bool())
-    val idex_rd      = Input(UInt(5.W))
-    val exmem_taken  = Input(Bool())
+    val rs1           = Input(UInt(5.W))
+    val rs2           = Input(UInt(5.W))
+    val idex_memread  = Input(Bool())
+    val idex_rd       = Input(UInt(5.W))
+    val exmem_taken   = Input(Bool())
+    val imem_good     = Input(Bool())
+    val dmem_good     = Input(Bool())
 
-    val imem_good    = Input(Bool())
-    val dmem_good    = Input(Bool())
-
-    val pcwrite      = Output(UInt(2.W))
-    val ifid_bubble  = Output(Bool())
-    val idex_bubble  = Output(Bool())
-    val exmem_bubble = Output(Bool())
-    val ifid_flush   = Output(Bool())
-
-    val idex_freeze  = Output(Bool())
-    val exmem_freeze = Output(Bool())
-    val memwb_freeze = Output(Bool())
+    val pcwrite       = Output(UInt(2.W))
+    val imem_disable  = Output(Bool())
+    val ifid_bubble   = Output(Bool())
+    val ifid_disable  = Output(Bool())
+    val ifid_flush    = Output(Bool())
+    val idex_bubble   = Output(Bool())
+    val idex_disable  = Output(Bool())
+    val exmem_bubble  = Output(Bool())
+    val exmem_disable = Output(Bool())
   })
 
   // default
   io.pcwrite      := 0.U
-  io.ifid_bubble  := false.B
-  io.idex_bubble  := false.B
-  io.exmem_bubble := false.B
-  io.ifid_flush   := false.B
-
-  io.idex_freeze  := false.B
-  io.exmem_freeze := false.B
-  io.memwb_freeze := false.B
+  io.imem_disable  = false.B
+  io.ifid_bubble   = false.B
+  io.ifid_disable  = false.B
+  io.ifid_flush    = false.B
+  io.idex_bubble   = false.B
+  io.idex_disable  = false.B
+  io.exmem_bubble  = false.B
+  io.exmem_disable = false.B
 
   // Load to use hazard.
   when (io.idex_memread &&
@@ -70,18 +72,29 @@ class HazardUnitMemStall extends Module {
   // branch flush
   when (io.exmem_taken && io.imem_good && io.dmem_good) {
     io.pcwrite := 1.U // use the PC from mem stage
-    io.ifid_flush  := true.B
+    io.ifid_flush   := true.B
     io.idex_bubble  := true.B
     io.exmem_bubble := true.B
   }
 
-  // mem stall
-  when (! (io.imem_good && io.dmem_good)) {
-    io.pcwrite := 2.U
+  // imem stall:
+  // Freeze the PC to preserve the current PC for the entire stall period,
+  // so that on un-stall the next PC points to the next instruction
+  when (! io.imem_good) {
+    io.pcwrite := 2.U // Freeze the PC so whenever possible the CPU re-executes the same instruction
+  }
 
-    io.ifid_bubble := true.B
-    io.idex_freeze := true.B
-    io.exmem_freeze := true.B
-    io.memwb_freeze := true.B
+  // dmem stall
+  // Freeze the PC to preserve the current PC for imem
+  // Disable any outbound instruction memory requests
+  // Disable writes for IF/ID
+  // Disable writes for ID/EX
+  // Disable writes for EX/MEM
+  when (! io.dmem_good) {
+    io.pcwrite := 2.U
+    io.imem_disable  := true.B
+    io.ifid_disable  := true.B
+    io.idex_disable  := true.B
+    io.exmem_disable := true.B
   }
 }
