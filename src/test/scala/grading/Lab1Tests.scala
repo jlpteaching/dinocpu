72eb1b562f5e698fa749962d3ac826f7c08fdee0
JulianToya <jtangeles@ucdavis.edu>
2020-01-06 11:50:42 -0800

    Bring parity to add tests and ignoring them in All r-type for Lab 1

--- src/test/scala/grading/Lab1Tests.scala
+++ src/test/scala/grading/Lab1Tests.scala
@@ -68,29 +68,6 @@ class Lab1Grader extends JUnitSuite {
   }
 
   @Test
-  @GradedTest(name="All R types", max_score=15)
-  def verifyRType() {
-    // Capture all of the console output from the test
-    val stream = new java.io.ByteArrayOutputStream()
-    Console.withOut(stream) {
-
-      implicit val conf = new CPUConfig()
-
-      var success = true
-      for (test <- InstTests.rtype) {
-        if (test.binary !== "add0") {
-          success = CPUTesterDriver(test, "single-cycle") && success
-        }
-      }
-
-      // Dump the output of the driver above onto the system out so that the
-      // gradescope function will catch it.
-      System.out.print(stream)
-      if (!success) fail("Test failed!")
-    }
-  }
-
-  @Test
   @GradedTest(name="Add instruction add0", max_score=5)
   def verifyAdd0() {
     // Capture all of the console output from the test
@@ -103,6 +80,29 @@ class Lab1Grader extends JUnitSuite {
       val test = InstTests.nameMap("add0")
 
       success = CPUTesterDriver(test, "single-cycle")
+
+      // Dump the output of the driver above onto the system out so that the
+      // gradescope function will catch it.
+      System.out.print(stream)
+      if (!success) fail("Test failed!")
+    }
+  }
+
+  @Test
+  @GradedTest(name="All R types", max_score=15)
+  def verifyRType() {
+    // Capture all of the console output from the test
+    val stream = new java.io.ByteArrayOutputStream()
+    Console.withOut(stream) {
+
+      implicit val conf = new CPUConfig()
+
+      var success = true
+      for (test <- InstTests.rtype) {
+        if (test.binary != "add0" && test.binary != "add1" && test.binary != "add2") {
+          success = CPUTesterDriver(test, "single-cycle") && success
+        }
+      }
 
       // Dump the output of the driver above onto the system out so that the
       // gradescope function will catch it.
