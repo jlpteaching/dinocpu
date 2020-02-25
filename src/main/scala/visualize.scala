7fd577e37741ab362723dd5d0f6672911f885074
klanmiko <eternity01033@gmail.com>
2020-02-24 21:21:04 -0800

    add comments explaining solution

--- src/main/scala/visualize.scala
+++ src/main/scala/visualize.scala
@@ -11,23 +11,29 @@ import javax.swing.ToolTipManager
 import java.awt.Color
 import treadle.TreadleTester
 
+// Port interface used by View to describe I/O ports on modules
 trait Port extends Publisher {
   val module: CircuitModule
   val name: String
   var value: BigInt
 }
 
+// Model implementation of Port interface loaded from FIRRTL Circuit
 class FIRRTLPort(n: String, bN: String, m: FIRRTLModule) extends Port {
   override val module:FIRRTLModule = m
   override val name = n
   override var value: BigInt = 0
+
+  // bindingName is the fully qualified name in the simulator SymbolTable
   val bindingName = bN
 }
 
+// Emitted by Port to notify View the value has changed
 object ValueChange extends Event {
 
 }
 
+// PortView is a Component that displays port information as text
 class PortView(input: Port) extends Label {
   text = input.name
   tooltip = input.value.toString
@@ -37,11 +43,13 @@ class PortView(input: Port) extends Label {
   }
 }
 
+// DinoWire is an abstract type that describes connections between modules
 trait DinoWire {
   val from: Port
   val to: Port  
 }
 
+// Static helper methods for constructing wires
 object DinoWire {
   def wireFromPorts(t: Port, f: Port): DinoWire = {
     new DinoWire {
@@ -56,6 +64,7 @@ trait Dimension2D {
   val height: Int
 }
 
+// CircuitModule is an interface for a module that contains a name and a list of I/O ports
 trait CircuitModule {
   val label: String
   val input: Seq[Port]
@@ -68,11 +77,16 @@ trait CircuitModule {
     }
 }
 
+// FIRRTLModule is an extension of CircuitModule that uses FIRRTLPorts
+// this is to expose the bindingName to the module
 trait FIRRTLModule extends CircuitModule {
   val input: Seq[FIRRTLPort]
   val output: Seq[FIRRTLPort]
 }
 
+/* Module View is a component that displays a module
+ * it contains a title row for the name and flexible layout for
+ * input ports in the left column, and output ports in a right column */
 class ModuleView(module: CircuitModule) extends BorderPanel {
   private val center = new BoxPanel(Orientation.Horizontal) {
     private val inputs = new BoxPanel(Orientation.Vertical) {
@@ -88,6 +102,8 @@ class ModuleView(module: CircuitModule) extends BorderPanel {
     private val outputs = new BoxPanel(Orientation.Vertical) {
       for(input <- module.output) {
         val label = new PortView(input)
+
+        // align the output labels to the right of its BoxLayout
         label.xLayoutAlignment = 1.0
         contents += label
       }
@@ -116,6 +132,7 @@ object CircuitStep extends Event {
   val why = ""
 }
 
+// CircuitModel is a top-level interface that gets passed into the UI Component to build the view hierarchy
 trait CircuitModel extends Publisher {
   val if_id: Seq[CircuitModule]
   val id_ex: Seq[CircuitModule]
@@ -133,6 +150,10 @@ trait CircuitModel extends Publisher {
   var cycle: BigInt
 }
 
+/* FIRRTLModel constructs a representation of a FIRRTL Circuit as a pipelined CPU,
+ * Given a list of modules for each pipeline register, and a list of modules in the CPU,
+ * the constructor assigns all the modules to the pipeline stages.  
+ */
 class FIRRTLModel(cpuModules: Seq[FIRRTLModule], connections_arg: Seq[DinoWire], 
   if_id_arg: Seq[FIRRTLModule], id_ex_arg: Seq[FIRRTLModule], ex_mem_arg: Seq[FIRRTLModule], mem_wb_arg: Seq[FIRRTLModule]) extends CircuitModel {
   override val connections = connections_arg
@@ -141,6 +162,11 @@ class FIRRTLModel(cpuModules: Seq[FIRRTLModule], connections_arg: Seq[DinoWire],
   override val ex_mem = ex_mem_arg
   override val mem_wb = mem_wb_arg
   
+  /* DFS starting at right Port to find a target module
+   * a module is reachable from a target port if a DFS can find a path between
+   * that port and any port in the query. The path is traversed over all the connections
+   * going backwards from "to" to "from". 
+   */
   def isLeftOfPort(query: CircuitModule, right: Port, seen: Set[Port]): Boolean = seen.contains(right) match {
     case false => {
       val cons = connections.filter(c => right == c.to)
@@ -149,9 +175,11 @@ class FIRRTLModel(cpuModules: Seq[FIRRTLModule], connections_arg: Seq[DinoWire],
     case true => false
   }
 
+  // A module is to the left of a target if any of the query ports are reachable from any of input ports of the target
   def isLeftOf(query: CircuitModule, right: CircuitModule): Boolean =
     right.input.exists(p => isLeftOfPort(query, p, HashSet[Port]()))
   
+  // the two following functions are defined as the opposite of the above
   def isRightOfPort(query: CircuitModule, left: Port, seen: Set[Port]): Boolean = seen.contains(left) match {
     case false => {
       val cons = connections.filter(c => left == c.from)
@@ -169,7 +197,11 @@ class FIRRTLModel(cpuModules: Seq[FIRRTLModule], connections_arg: Seq[DinoWire],
   override val executeModules = new mutable.ArrayBuffer[FIRRTLModule]
   override val memoryModules = new mutable.ArrayBuffer[FIRRTLModule]
   override val writeBackModules = new mutable.ArrayBuffer[FIRRTLModule]
-
+ 
+  /* iteratively add each of the cpuModules to a pipeline stage
+   * a module is in a stage if it is to the left of any of the next pipeline registers
+   * or to the right of any of the previous pipeline register
+   */
   cpuModules.foreach(mod => {
     if (if_id.exists(reg => isLeftOf(mod, reg)))
       fetchModules += mod
@@ -189,11 +221,15 @@ class FIRRTLModel(cpuModules: Seq[FIRRTLModule], connections_arg: Seq[DinoWire],
   def loadFromDriver(driver: CPUTesterDriver) {
     val simulator = driver.simulator
 
+    // peek all the ports within the circuit hierarchy and publish ValueChaged events if needed
     for(module <- toCheck) {
       for(port <- module.input ++ module.output) {
         try {
+          val oldVal = port.value
           port.value = simulator.peek(port.bindingName)
-          port.publish(ValueChange)
+          
+          if(oldVal != port.value)
+            port.publish(ValueChange)
         } catch {
           case e: AssertionError => {
             /* val symbol = table.get(port.bindingName)
@@ -299,6 +335,12 @@ object FIRRTLModel {
       cpuModules += processDeclaration(r, inCPU)
     }
 
+    /* processNode attempts to convert a node (mux, combinational logic) into a module
+     * by definining an input and output port with the same name,
+     * wiring all dependencies into the input side from the definition
+     * wiring the input to the output (allows for DFS)
+     * anywhere this node is used a wire will be created from the output side
+     */
     def processNode(r: DefNode, inCPU: Boolean = false) {
       val module = processDeclaration(r, inCPU)
 
@@ -325,6 +367,7 @@ object FIRRTLModel {
       cpuNodes += processExpression(r.value, module)
     }
 
+    // processInstance adds a module definition to the list of CPUModules
     def processInstance(i: DefInstance, inCPU: Boolean = false) {
       def expand(moduleName: String, portName: String): String = inCPU match {
         case false => moduleName + "." + portName 
@@ -342,6 +385,7 @@ object FIRRTLModel {
       cpuModules += new_module
     }
 
+    // processConnect adds wires between dependencies on the right hand side to the port on the left hand side
     def processConnect(c: Connect) {
       val fromPort = processFromExpression(c.expr)
       val toPort = processToExpression(c.loc)
@@ -395,7 +439,10 @@ object FIRRTLModel {
     val mem_wb = cpuModules.find(mod => mod.label == "mem_wb").getOrElse(moduleNotFound)
     val mem_wb_ctrl = cpuModules.find(mod => mod.label == "mem_wb_ctrl").getOrElse(moduleNotFound)
 
+    // removeNode removes a module from the network graph adding connections from all modules
+    // wired to inputs of the node to all modules wired to outputs of the node
     def removeNode(node: FIRRTLModule) {
+      // filter out the node's circular from-to wire from dependencies
       val toNode = connections.filter(c => c.to.module == node && c.from.module != node)
       val fromNode = connections.filter(c => c.from.module == node && c.to.module != node)
 
@@ -408,6 +455,7 @@ object FIRRTLModel {
       connections --= toNode
       connections --= fromNode
 
+      // delete the node's from-to wire here
       connections --= connections.filter(c => c.to.module == node && c.from.module == node)
     }
 
@@ -481,12 +529,15 @@ class Circuit(model: CircuitModel) extends BoxPanel(Orientation.Horizontal) {
   }
 }
 
-
+// UI is the top-level view component that constructs the view hierarchy from a CircuitModel
 class UI(model: CircuitModel) extends MainFrame {
   val stepButton = new Button("Step")
   val cycleCount = new Label("Cycle: " + model.cycle.toString)
 
+  // mainPane contains the rendered circuit
   val mainPane = new SplitPane(Orientation.Vertical)
+
+  // controlPane displays the step button and cycle count
   val controlPane = new BoxPanel(Orientation.Vertical)
   val circuitPane = new ScrollPane
 
@@ -517,6 +568,7 @@ class UI(model: CircuitModel) extends MainFrame {
   }
 }
 
+// SimulationController maps the ui step button to trigger the Model to update
 class SimulationController(driver: CPUTesterDriver, model: FIRRTLModel, ui: UI) {
   def step() {
     driver.step(1)
